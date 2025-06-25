ec8642a9a1b5151f23eb3ab11dd167739c26cc19
Jacob Qumsiyeh <38016471+Jacob11Q1@users.noreply.github.com>
2025-06-25 23:03:13 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("page loaded...");
+function over(video){
+    video.play();
+}
+
+function unMute(element){
+    element.muted = false;
+}
+
+function out(video){
+    video.pause();
+}
